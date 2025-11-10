2403453a80bf65603161f6741dbbb9a470085850
JesusRosdan <jesusrosdan@hotmail.com>
2025-11-09 23:01:37 -0600

    CSS files and improved HTML files structure

--- appFlask/primera/app1.py
+++ appFlask/primera/app1.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
